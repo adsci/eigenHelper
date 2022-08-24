19435aed879b62167da28b884c3b561fe22fd9a8
Adam Sciegaj <adam.sciegaj@gmail.com>
2022-08-24 12:40:17 +0200

    Added Node and NodeSet classes

--- eigenHelper/main.py
+++ eigenHelper/main.py
@@ -1,11 +1,33 @@
 from bokeh.io import curdoc
-from bokeh.models import Div
+from bokeh.models import Div, NumericInput, Button
+from bokeh.layouts import column, row, Spacer
+from functools import partial
+from utils import *
 
 def modify_doc(doc):
-    div = Div(text="""
-    eigenHelper is a browser-based app visualising eigenmodes of simple frame structures.
-    The app is intented to be used by students learning to solve eigenvalue problems for 
-    free vibration of frame structures""", width=300, height=100)
-    doc.add_root(div)
+    def addNodeOnClick(nodeset, xCoordInput, yCoordInput, dtext):
+        nodeset.addNode(float(xCoordInput.value),float(yCoordInput.value))
+        dtext.text=nodeset.__str__()
+
+    def delNodeOnClick(nodeset, delNodeInput, dtext):
+        if delNodeInput.value <= 0 or delNodeInput.value > nodeset.getNumberOfNodes():
+            return
+        nodeset.deleteNodeWithID(delNodeInput.value)
+        dtext.text=nodeset.__str__()
+
+    nset = NodeSet()
+    nX = NumericInput(value=0, title="Enter x coordinate:",mode='float', width=75,height=50)
+    nY = NumericInput(value=0, title="Enter y coordinate:",mode='float', width=75)
+    delNodeNum = NumericInput(value=0, title="Node to be deleted:",mode='int', width=75)
+    addNodeButton = Button(label="Add Node", button_type="primary", width=120 )
+    delNodeButton = Button(label="Delete Node", button_type="danger", width=120 )
+    div = Div(text=nset.__str__(), width=150, height=300)
+
+    #handlers
+    addNodeButton.on_click(partial(addNodeOnClick, nodeset=nset, xCoordInput=nX, yCoordInput=nY, dtext=div))
+    delNodeButton.on_click(partial(delNodeOnClick, nodeset=nset, delNodeInput=delNodeNum, dtext=div))
+
+    layout = row(column(nX,nY, addNodeButton), column(Spacer(width=100,height=nX.height+10), delNodeNum, delNodeButton), div)
+    doc.add_root(layout)
 
 modify_doc(curdoc())
---
+++ eigenHelper/utils.py
@@ -0,0 +1,60 @@
+class NodeSet():
+    def __init__(self):
+        self.nodes = []
+
+    def __str__(self):
+        desc = "<b>Nodes</b>:<br>"
+        for node in self.nodes:
+            desc += node.__str__() + "<br>"
+        return desc
+
+    def getNumberOfNodes(self):
+        return len(self.nodes)
+
+    def addNode(self,x,y):
+        if not self.found(x,y):
+            self.nodes.append(Node(x,y,self.getNextID()))
+
+    def getNextID(self):
+        return len(self.nodes) + 1
+
+    def found(self,x,y):
+        for node in self.nodes:
+            if (abs(node.getX() - x) <= 1e-6) and (abs(node.getY() - y) < 1e-6):
+                return True
+        return False
+
+    def renumber(self,fromID):
+        for node in self.nodes:
+            if node.getID() <= fromID:
+                continue
+            else:
+                node.setID(node.getID()-1)
+
+    def deleteNodeWithID(self,id):
+        assert self.nodes[id-1].getID() == id
+        del self.nodes[id-1]
+        self.renumber(id)
+
+class Node():
+    def __init__(self,x,y,id):
+        self.coords=[x,y]
+        self.id = id
+
+    def __str__(self):
+        return "Node " +str(self.getID()) + " at " + str(self.getCoords())
+
+    def getID(self):
+        return self.id
+
+    def getX(self):
+        return self.coords[0]
+
+    def getY(self):
+        return self.coords[1]
+
+    def getCoords(self):
+        return self.coords
+
+    def setID(self,newID):
+        self.id = newID
